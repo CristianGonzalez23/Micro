a8c53768174600b26e135accd74ddc3b943f7201
CristianGonzalez23 <cristianr.gonzalezi@uqvirtual.edu.co>
2024-09-14 11:14:23 -0500

    completo

--- OpenApi/init.sql
+++ OpenApi/init.sql
@@ -18,3 +18,19 @@ CREATE TABLE usuarios (
     is_deleted BOOLEAN DEFAULT FALSE,
     last_login TIMESTAMP
 );
+INSERT INTO usuarios (nombre, email, password, is_deleted, last_login) VALUES
+('John Doe', 'john.doe@example.com', 'password1', FALSE, NOW()),
+('Emily Smith', 'emily.smith@example.com', 'password2', FALSE, NOW()),
+('Michael Johnson', 'michael.johnson@example.com', 'password3', FALSE, NOW()),
+('Sophia Williams', 'sophia.williams@example.com', 'password4', FALSE, NOW()),
+('David Brown', 'david.brown@example.com', 'password5', FALSE, NOW()),
+('Olivia Jones', 'olivia.jones@random.com', 'pass123', FALSE, NOW()),
+('William Miller', 'william.miller@random.com', 'qwerty01', FALSE, NOW()),
+('Ashley Hernandez', 'ashley.hernandez@random.com', 'secret123', FALSE, NOW()),
+('Charles Garcia', 'charles.garcia@random.com', '!@#$%^,', FALSE, NOW()),
+('Jessica Moore', 'jessica.moore@random.com', 'welcome123', FALSE, NOW()),
+('Christopher Taylor', 'christopher.taylor@random.com', 'dragonfire', FALSE, NOW()),
+('Amanda Davis', 'amanda.davis@random.com', 'sunshine1', FALSE, NOW()),
+('Daniel Martin', 'daniel.martin@random.com', 'player1', FALSE, NOW()),
+('Sarah Allen', 'sarah.allen@random.com', 'hello123', FALSE, NOW()),
+('Nicholas Rodriguez', 'nicholas.rodriguez@random.com', 'password007', FALSE, NOW());
